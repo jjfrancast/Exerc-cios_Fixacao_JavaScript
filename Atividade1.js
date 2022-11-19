ca38f907abca922bb1de52147b12652b9ade7803
Perkles <jonastorres1993@gmail.com>
2022-11-19 18:41:55 -0300

    exercicios de fixacao finalizados

---
+++ Atividade1.js
@@ -0,0 +1,21 @@
+/*
+Faça um programa que receba a média de um aluno:
+Caso a média seja <5 imprima "Reprovado";
+Caso a média seja >= e <7 imprima "Recuperação"
+Caso a média seja >= 7 imprima "Aprovado"
+*/
+
+const { gets, print } = require('./Ex1.js');
+const media = gets();
+
+if (media >= 0 && media < 5) {
+    print('Reprovado');
+}else if (media >= 5 && media < 7) {
+    print('Recuperação');
+}else if (media => 7){
+    print('Aprovado');
+}else{
+    print('Entrada Inválida');
+}
+
+
